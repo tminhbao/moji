ea6df377a9f6f45269db4127b162f6306bde31b6
tminhbao <tminhbao2001@gmail.com>
2026-05-14 16:34:32 +0700

    backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./libs/db";
+import { connectDB } from "./libs/db.js";
+import authRoute from "./routes/authRoute.js";
+import cookieParser from "cookie-parser";
+import { setServers } from "node:dns/promises";
+import { protectedRoute } from "./middlewares/authMiddleware.js";
 
 dotenv.config();
 
@@ -9,7 +13,15 @@ const PORT = process.env.PORT || 3001;
 
 // middlewares
 app.use(express.json());
+app.use(cookieParser());
 
+// public routes
+app.use("/api/auth", authRoute);
+// private routes
+app.use(protectedRoute);
+app.use("/api/user", authRoute);
+
+setServers(["1.1.1.1", "8.8.8.8"]);
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
